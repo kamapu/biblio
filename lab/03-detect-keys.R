2828a238d048440af89893e3d9fa377e24fcb2dc
Miguel Alvarez <kamapu78@gmail.com>
2022-07-28 16:32:06 +0200

    Some uncommited files

---
+++ lab/03-detect-keys.R
@@ -0,0 +1,9 @@
+# TODO:   Add comment
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+library(biblio)
+library(yamlme)
+
+detect_keys(read_rmd("lab/test-file.Rmd"))
