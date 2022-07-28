6497260a54c9b1fd50d7189a7c10a11331683353
Miguel Alvarez <kamapu78@gmail.com>
2022-07-28 15:07:38 +0200

    New version of function 'detect_keys()'

--- R/compare_df.R
+++ R/compare_df.R
@@ -1,111 +1,131 @@
 #' @name compare_df
-#' 
+#'
 #' @rdname compare_df
-#' 
+#'
 #' @title Compare data frames and libraries
-#' 
-#' @description 
+#'
+#' @description
 #' Report on differences between two versions of the same data frame or
 #' electronic library.
 #' When used for data frames, you need to indicate the variable containing IDs
 #' for each entry, while applied to [lib_df-class] objects, the variable
 #' 'bibtexkey' will be considered as ID per default.
-#' 
+#'
 #' The output printed in the console will advice about added and deleted entries
 #' in 'y' as well as any change in the entries common to both versions.
-#' 
+#'
 #' @param x The reference data frame.
 #' @param y The updated data frame.
 #' @param key A character value with the name of the variable used as primary
 #'     key in the tables.
 #' @param ... Further arguments passed among methods.
-#' 
-#' @seealso 
+#'
+#' @seealso
 #' [biblio::update()], [lib_df-class], [comp_df-class]
-#' 
-#' @return 
+#'
+#' @return
 #' A S3 object of class [comp_df-class], which can be printed in the console by
 #' [biblio::print()].
-#' 
-#' @examples 
+#'
+#' @examples
 #' # Partially matching libraries
 #' Refs1 <- synopsis[1:10, ]
 #' Refs2 <- synopsis[6:15, ]
-#' 
+#'
 #' # some modification in second library
 #' Refs2[3, "title"] <- "New Title"
-#' 
+#'
 #' # compare libraries
 #' compare_df(Refs1, Refs2)
-#' 
+#'
 #' @exportMethod compare_df
-#' 
-setGeneric("compare_df",
-		function(x, y, key, ...)
-			standardGeneric("compare_df")
+#'
+setGeneric(
+  "compare_df",
+  function(x, y, key, ...) {
+    standardGeneric("compare_df")
+  }
 )
 
 #' @rdname compare_df
-#' 
+#'
 #' @aliases compare_df,data.frame,data.frame,character-method
-#' 
-setMethod("compare_df", signature(x = "data.frame", y = "data.frame",
-				key = "character"), function(x, y, key, ...) {
-			# Compare variables in data frames
-			common_cols <- intersect(colnames(x), colnames(y))
-			## if(!setequal(colnames(x), colnames(y)))
-			if(any(!colnames(y) %in% colnames(x)))
-				warning(paste("Some columns of 'y' are not in 'x'.",
-								"Only common variables will be compared"))
-			if(any(!colnames(x) %in% colnames(y)))
-				warning(paste("Some columns of 'x' are not in 'y'.",
-								"Only common variables will be compared"))
-			# Other checks
-			if(!key %in% common_cols)
-				stop(paste("The value of 'key' is missing in the columns",
-								"of at least one input data frames."))
-			if(any(duplicated(x[ ,key])))
-				stop(paste("Duplicated key values found in 'x'."))
-			if(any(duplicated(y[ ,key])))
-				stop(paste("Duplicated key values found in 'y'."))
-			# Compare entries
-			rownames(x) <- paste(x[ ,key])
-			rownames(y) <- paste(y[ ,key])
-			common_keys <- intersect(x[ ,key], y[ ,key])
-			added <- y[!y[ ,key] %in% common_keys, ]
-			deleted <- x[!x[ ,key] %in% common_keys, key]
-			# Updates
-			common_cols <- common_cols[common_cols != key]
-			# Function to handle NAs in comparisons
-			compareNA <- function(x, y) {
-				same <- (x == y) | (is.na(x) & is.na(y))
-				same[is.na(same)] <- FALSE
-				return(!same)
-			}
-			x <- x[paste(common_keys), common_cols]
-			y <- y[paste(common_keys), common_cols]
-			updated <- x
-			for(i in common_cols)
-				updated[ ,i] <- compareNA(x[ ,i], y[, i])
-			updated <- as.matrix(updated)
-			row_sums <- rowSums(updated)
-			col_sums <- colSums(updated)
-			OUT <- list(added = added, deleted = deleted,
-					updated = updated[row_sums > 0, col_sums > 0, drop = FALSE],
-					old_vals = x[row_sums > 0,col_sums > 0, drop = FALSE],
-					new_vals = y[row_sums > 0,col_sums > 0,drop=FALSE])
-			class(OUT) <- c("comp_df", "list")
-			return(OUT)
-		})
+#'
+setMethod("compare_df", signature(
+  x = "data.frame", y = "data.frame",
+  key = "character"
+), function(x, y, key, ...) {
+  # Compare variables in data frames
+  common_cols <- intersect(colnames(x), colnames(y))
+  ## if(!setequal(colnames(x), colnames(y)))
+  if (any(!colnames(y) %in% colnames(x))) {
+    warning(paste(
+      "Some columns of 'y' are not in 'x'.",
+      "Only common variables will be compared"
+    ))
+  }
+  if (any(!colnames(x) %in% colnames(y))) {
+    warning(paste(
+      "Some columns of 'x' are not in 'y'.",
+      "Only common variables will be compared"
+    ))
+  }
+  # Other checks
+  if (!key %in% common_cols) {
+    stop(paste(
+      "The value of 'key' is missing in the columns",
+      "of at least one input data frames."
+    ))
+  }
+  if (any(duplicated(x[, key]))) {
+    stop(paste("Duplicated key values found in 'x'."))
+  }
+  if (any(duplicated(y[, key]))) {
+    stop(paste("Duplicated key values found in 'y'."))
+  }
+  # Compare entries
+  rownames(x) <- paste(x[, key])
+  rownames(y) <- paste(y[, key])
+  common_keys <- intersect(x[, key], y[, key])
+  added <- y[!y[, key] %in% common_keys, ]
+  deleted <- x[!x[, key] %in% common_keys, key]
+  # Updates
+  common_cols <- common_cols[common_cols != key]
+  # Function to handle NAs in comparisons
+  compareNA <- function(x, y) {
+    same <- (x == y) | (is.na(x) & is.na(y))
+    same[is.na(same)] <- FALSE
+    return(!same)
+  }
+  x <- x[paste(common_keys), common_cols]
+  y <- y[paste(common_keys), common_cols]
+  updated <- x
+  for (i in common_cols) {
+    updated[, i] <- compareNA(x[, i], y[, i])
+  }
+  updated <- as.matrix(updated)
+  row_sums <- rowSums(updated)
+  col_sums <- colSums(updated)
+  OUT <- list(
+    added = added, deleted = deleted,
+    updated = updated[row_sums > 0, col_sums > 0, drop = FALSE],
+    old_vals = x[row_sums > 0, col_sums > 0, drop = FALSE],
+    new_vals = y[row_sums > 0, col_sums > 0, drop = FALSE]
+  )
+  class(OUT) <- c("comp_df", "list")
+  return(OUT)
+})
 
 #' @rdname compare_df
-#' 
+#'
 #' @aliases compare_df,lib_df,lib_df,missing-method
-#' 
-setMethod("compare_df", signature(x = "lib_df", y = "lib_df",
-				key = "missing"), function(x, y, ...) {
-			key <- "bibtexkey"
-			class(x) <- "data.frame"
-			class(y) <- "data.frame"
-			compare_df(x = x, y = y, key = key, ...)
-		})
+#'
+setMethod("compare_df", signature(
+  x = "lib_df", y = "lib_df",
+  key = "missing"
+), function(x, y, ...) {
+  key <- "bibtexkey"
+  class(x) <- "data.frame"
+  class(y) <- "data.frame"
+  compare_df(x = x, y = y, key = key, ...)
+})
--- R/detect_keys.R
+++ R/detect_keys.R
@@ -1,77 +1,84 @@
 #' @name detect_keys
-#' 
+#'
 #' @rdname detect_keys
-#' 
+#'
 #' @title Detect bibtexkeys used in an r-markdown document
-#' 
-#' @description 
+#'
+#' @description
 #' This function screens a character vector (usually an imported r-markdown
 #' document) for the use of citations by bibtexkeys (`@@bibtexkey`), retrieving
 #' the detected key with its occurrence in the vector, assuming each element as
 #' a line of the original document.
-#' 
+#'
 #' This function is based on `bbt_detect_citations()` from the package
 #' [rbbt](https://github.com/paleolimbot/rbbt).
-#' 
+#'
 #' @param x A character vector, a file imported by [readLines()] or an object
 #'     imported by [read_rmd()]. If the character vector is the name of a Rmd
 #'     file, [readLines()] will be internally called to read it.
 #' @param ... Further arguments passed among methods. In character-method they
 #'     are passed to [readLines()].
-#' 
-#' @return 
+#'
+#' @return
 #' A data frame with two columns, `bibtexkey` for the found keys and `line`
 #' with the line number of the occurrence of the key in the document.
-#' 
-#' @examples 
+#'
+#' @examples
 #' ## Read installed r-markdown document
 #' my_document <- readLines(file.path(path.package("biblio"), "document.Rmd"))
-#' 
+#'
 #' ## Screen for citations
 #' cited_refs <- detect_keys(my_document)
 #' cited_refs
-#' 
+#'
 #' @export detect_keys
-#' 
-detect_keys <- function (x, ...) {
-	UseMethod("detect_keys", x)
+#'
+detect_keys <- function(x, ...) {
+  UseMethod("detect_keys", x)
 }
 
 #' @rdname detect_keys
-#' 
+#'
 #' @aliases detect_keys,character-method
-#' 
+#'
 #' @method detect_keys character
-#' @export 
-#' 
+#' @export
+#'
 detect_keys.character <- function(x, ...) {
-	# If character the name of a file
-	if(length(x) == 1 & substr(x[1], nchar(x) - 3, nchar(x)) == ".Rmd")
-		x <- readLines(x, ...)
-	# Code from rbbt::detect_citations()
-	match_str <- function(content)
-		str_match_all(
-				content,
-				regex("[^a-zA-Z0-9\\\\]@([a-zA-Z0-9_.-]+[a-zA-Z0-9])",
-						multiline = TRUE, dotall = TRUE))[[1]][ , 2,
-				drop =  TRUE]
-	# keys and lines
-	Keys <- lapply(x, match_str)
-	Lines <- do.call(c, lapply(Keys, length))
-	Lines <- rep(seq_along(Lines), times = Lines)
-	Keys <- do.call(c, Keys)
-	# Final output
-	return(data.frame(bibtexkey = Keys, line = Lines))
+  # If character the name of a file
+  if (length(x) == 1 & substr(x[1], nchar(x) - 3, nchar(x)) == ".Rmd") {
+    x <- readLines(x, ...)
+  }
+  # Code from rbbt::detect_citations()
+  match_str <- function(content, exp) {
+    str_match_all(content, regex(exp, multiline = TRUE, dotall = TRUE))[[1]][,
+      2,
+      drop = TRUE
+    ]
+  }
+  # keys and lines
+  do_df <- function(content, exp) {
+    Keys <- lapply(X = content, match_str, exp = exp)
+    Lines <- do.call(c, lapply(Keys, length))
+    Lines <- rep(seq_along(Lines), times = Lines)
+    Keys <- do.call(c, Keys)
+    return(data.frame(bibtexkey = Keys, line = Lines))
+  }
+  # Expressions (second solving issue 32)
+  exp1 <- "[^a-zA-Z0-9\\\\]@([a-zA-Z0-9_.-]+[a-zA-Z0-9])"
+  exp2 <- "^@([a-zA-Z0-9_.-]+[a-zA-Z0-9])"
+  # Final output
+  return(do.call(rbind, list(do_df(x, exp1), do_df(x, exp2))))
 }
 
 #' @rdname detect_keys
-#' 
+#'
 #' @aliases detect_keys,rmd_doc-method
-#' 
+#'
 #' @method detect_keys rmd_doc
-#' @export 
-#' 
+#' @export
+#'
 detect_keys.rmd_doc <- function(x, ...) {
-	message("Lines are counted only at the body of the document.")
-	detect_keys(x[["body"]], ...)
+  message("Lines are counted only at the body of the document.")
+  detect_keys(x[["body"]], ...)
 }
