487b65fade7e976a51c3af3f44fb68c8b353ae1d
Miguel Alvarez <kamapu78@gmail.com>
2024-04-13 00:34:28 +0200

    Print method for 'lib_df' objects was enhanced.

--- examples/coerce-methods.R
+++ examples/coerce-methods.R
@@ -1,9 +1,9 @@
 ## Read installed library
-Bib <- read_bib(x = file.path(path.package("biblio"),
+Refs <- read_bib(x = file.path(path.package("biblio"),
         "LuebertPliscoff.bib"))
 
 # Convert lib_df to data frame
-as(Bib[1:5, ], "data.frame")
+as(Refs[1:5, ], "data.frame")
 
 # Convert lib_df to bibentry
-as(Bib[1:5, ], "bibentry")
+as(Refs[1:5, ], "bibentry")
--- lab/exp-coerce-methods.R
+++ lab/exp-coerce-methods.R
@@ -8,7 +8,6 @@ library(biblio)
 Bib <- read_bib(x = file.path(path.package("biblio"),
         "LuebertPliscoff.bib"))
 
-
 as(Bib[1:5, ], "data.frame")
 
 as(Bib[1:5, ], "bibentry")
