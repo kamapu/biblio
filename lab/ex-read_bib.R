a8e14d63a54b4e703c564f990330e5333f50f1b3
Miguel Alvarez <kamapu78@gmail.com>
2024-04-12 15:20:46 +0200

    Improved function 'read_bib()'.

---
+++ lab/ex-read_bib.R
@@ -0,0 +1,9 @@
+# TODO:   Add comment
+# 
+# Author: Miguel Alvarez
+################################################################################
+
+library(biblio)
+
+x <- readLines("lab/dummy-bib.bib")
+
