09b80092fb82a12a5d51a3a9699f5768b3923521
Miguel Alvarez <kamapu78@gmail.com>
2024-05-20 16:50:28 +0200

    Example added to lab.

--- lab/exp-read_bib.R
+++ lab/exp-read_bib.R
@@ -17,3 +17,19 @@ Bib
 Bib <- read_bib("lab/dummy-bib3.bib")
 Bib
 as(Bib, "data.frame")
+
+# Solving issue #49
+
+Refs <- read_bib(x = "lab/data/MiguelReferences.bib")
+Keys <- readLines("lab/data/Keys.txt")
+Refs <- subset(Refs, bibtexkey %in% Keys)
+write_bib(Refs, "lab/data/references")
+
+as(Refs, "data.frame")
+
+x <- subset(Refs, grepl("address =", address))
+as(x, "data.frame")
+
+
+
+
--- lab/exp-write_bib.R
+++ lab/exp-write_bib.R
@@ -11,3 +11,17 @@ Bib <- read_bib(x = file.path(path.package("biblio"),
 y2000 <- subset(Bib, year == "2000")
 
 write_bib(y2000, "lab/year_2000.bib")
+
+# New issue
+
+Refs <- read_bib(x = "lab/data/MiguelReferences.bib")
+Keys <- readLines("lab/data/Keys.txt")
+Refs <- subset(Refs, bibtexkey %in% Keys)
+write_bib(Refs, "lab/data/references")
+
+as(Refs, "data.frame")
+
+x <- subset(Refs, grepl("address =", address))
+as(x, "data.frame")
+
+
